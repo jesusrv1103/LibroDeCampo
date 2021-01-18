950b84f591e840f17339aa15a51c1ac1beaec8e1
Alberto Ortiz <beto@hacker.localdomain>
2021-01-17 22:01:13 -0600

    Validaciones

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.base')
+@include('layouts.menu')
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,9 +7,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cuaderno de Campo</title>
+    <title>Cuaderno de campo para Viña</title>
 </head>
 <body>
+    <div class="card">
 
+      <p>Campaña: <label for=""class="col-4"></label></p>
+      <fieldset>
+        <legend>DATOS DE IDENTIFICACION</legend>
+          <p>Titular de la explotacion: <label for=""class="col-4"></label></p>
+          <p>Direccion: <label for=""class="col-4"></label></p>
+          <p>Municipio: <label for=""class="col-4"></label></p>
+          <p>Tecnico responsable de la explotacio: <label for=""class="col-4"></label></p>
+      </fieldset>
+
+
+      <p>Fax: <label for=""class="col-4"></label></p>
+      <p>Codigo Postal: <label for=""class="col-4"></label></p>
+    </div>
 </body>
 </html>
