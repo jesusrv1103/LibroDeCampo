951cd1e82a121f5d707557e635e41d20eacc86a5
Alberto Ortiz <beto@hacker.localdomain>
2021-01-17 22:05:12 -0600

    Validaciones

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,20 +10,25 @@
     <title>Cuaderno de campo para Viña</title>
 </head>
 <body>
-    <div class="card">
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-7 mt-3">
+        <div class="card">
 
-      <p>Campaña: <label for=""class="col-4"></label></p>
-      <fieldset>
-        <legend>DATOS DE IDENTIFICACION</legend>
-          <p>Titular de la explotacion: <label for=""class="col-4"></label></p>
-          <p>Direccion: <label for=""class="col-4"></label></p>
-          <p>Municipio: <label for=""class="col-4"></label></p>
-          <p>Tecnico responsable de la explotacio: <label for=""class="col-4"></label></p>
-      </fieldset>
+          <p>Campaña: <label for=""class="col-4"></label></p>
+          <fieldset>
+            <legend>DATOS DE IDENTIFICACION</legend>
+            <p>Titular de la explotacion: <label for=""class="col-4"></label></p>
+            <p>Direccion: <label for=""class="col-4"></label></p>
+            <p>Municipio: <label for=""class="col-4"></label></p>
+            <p>Tecnico responsable de la explotacio: <label for=""class="col-4"></label></p>
+          </fieldset>
 
-
-      <p>Fax: <label for=""class="col-4"></label></p>
-      <p>Codigo Postal: <label for=""class="col-4"></label></p>
+          <p>Fax: <label for=""class="col-4"></label></p>
+          <p>Codigo Postal: <label for=""class="col-4"></label></p>
+        </div>
+      </div>
     </div>
+  </div>
 </body>
 </html>
